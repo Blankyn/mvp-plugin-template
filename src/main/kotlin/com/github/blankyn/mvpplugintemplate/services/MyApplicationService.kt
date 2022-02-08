e5dc753fa1b9c23758d7ac05a7b00e5abc0e9818
GitHub Action <action@github.com>
2022-02-08 01:13:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/blankyn/mvpplugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/blankyn/mvpplugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.blankyn.mvpplugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.blankyn.mvpplugintemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/blankyn/mvpplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/blankyn/mvpplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.blankyn.mvpplugintemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.blankyn.mvpplugintemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
